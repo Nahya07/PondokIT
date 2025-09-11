bf69547cc2a18f26bead987d5226c4a96c236be2
Nahya07 <59536043+Nahya07@users.noreply.github.com>
2025-09-12 01:23:20 +0700

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,1 +1,31 @@
+const CACHE_NAME = 'app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/index.html',
+  '/static/assets/icons/icon-192x192.png',
+  '/static/assets/icons/icon-512x512.png'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Jika respons ada di cache, gunakan itu.
+        if (response) {
+          return response;
+        }
+        // Jika tidak, ambil dari jaringan.
+        return fetch(event.request);
+      })
+  );
+});
